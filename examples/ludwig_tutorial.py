765029dc9f8dd4078dd09d577aea5c933cdc051b
TNTran92 <tran.aggies@gmail.com>
2024-03-23 01:20:53 -0500

    Get to work with ROCm

---
+++ examples/ludwig_tutorial.py
@@ -0,0 +1,190 @@
+import getpass
+#import locale; locale.getpreferredencoding = lambda: "UTF-8"
+import logging
+import os
+import torch
+import yaml
+import numpy as np; np.random.seed(123)
+import pandas as pd
+
+from ludwig.api import LudwigModel
+
+read_token = "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"
+os.environ["HUGGING_FACE_HUB_TOKEN"] = read_token
+assert os.environ["HUGGING_FACE_HUB_TOKEN"]
+
+df = pd.read_json("https://raw.githubusercontent.com/sahil280114/codealpaca/master/data/code_alpaca_20k.json")
+
+# We're going to create a new column called `split` where:
+# 90% will be assigned a value of 0 -> train set
+# 5% will be assigned a value of 1 -> validation set
+# 5% will be assigned a value of 2 -> test set
+# Calculate the number of rows for each split value
+total_rows = len(df)
+split_0_count = int(total_rows * 0.9)
+split_1_count = int(total_rows * 0.05)
+split_2_count = total_rows - split_0_count - split_1_count
+
+# Create an array with split values based on the counts
+split_values = np.concatenate([
+    np.zeros(split_0_count),
+    np.ones(split_1_count),
+    np.full(split_2_count, 2)
+])
+
+# Shuffle the array to ensure randomness
+np.random.shuffle(split_values)
+
+# Add the 'split' column to the DataFrame
+df['split'] = split_values
+df['split'] = df['split'].astype(int)
+
+# For this webinar, we will just 100 rows of this dataset.
+df = df.head(n=100)
+print(df.head(10))
+print('End of file')
+
+num_self_sufficient = (df['input'] == '').sum()
+num_need_contex = df.shape[0] - num_self_sufficient
+
+# We are only using 100 rows of this dataset for this webinar
+print(f"Total number of examples in the dataset: {df.shape[0]}")
+
+print(f"% of examples that are self-sufficient: {round(num_self_sufficient/df.shape[0] * 100, 2)}")
+print(f"% of examples that are need additional context: {round(num_need_contex/df.shape[0] * 100, 2)}")
+
+# Calculating the length of each cell in each column
+df['num_characters_instruction'] = df['instruction'].apply(lambda x: len(x))
+df['num_characters_input'] = df['input'].apply(lambda x: len(x))
+df['num_characters_output'] = df['output'].apply(lambda x: len(x))
+
+# Show Distribution
+df.hist(column=['num_characters_instruction', 'num_characters_input', 'num_characters_output'])
+
+# Calculating the average
+average_chars_instruction = df['num_characters_instruction'].mean()
+average_chars_input = df['num_characters_input'].mean()
+average_chars_output = df['num_characters_output'].mean()
+
+print(f'Average number of tokens in the instruction column: {(average_chars_instruction / 3):.0f}')
+print(f'Average number of tokens in the input column: {(average_chars_input / 3):.0f}')
+print(f'Average number of tokens in the output column: {(average_chars_output / 3):.0f}', end="\n\n")
+
+# Zero-shop Inference
+zero_shot_config = yaml.safe_load(
+  """
+  model_type: llm
+  base_model: meta-llama/Llama-2-7b-hf
+
+  input_features:
+    - name: instruction
+      type: text
+
+  output_features:
+    - name: output
+      type: text
+
+  prompt:
+    template: >-
+      Below is an instruction that describes a task, paired with an input
+      that may provide further context. Write a response that appropriately
+      completes the request.
+
+      ### Instruction: {instruction}
+
+      ### Input: {input}
+
+      ### Response:
+
+  generation:
+    temperature: 0.1 # Temperature is used to control the randomness of predictions.
+    max_new_tokens: 512
+
+  preprocessing:
+    split:
+      type: fixed
+
+  quantization:
+    bits: 4
+  """
+)
+
+# Just run on 10 examples for now
+model = LudwigModel(config=zero_shot_config, logging_level=logging.INFO)
+#results = model.train(dataset=df)
+
+# Fine-tune
+qlora_fine_tuning_config = yaml.safe_load(
+"""
+model_type: llm
+base_model: meta-llama/Llama-2-7b-hf
+
+input_features:
+  - name: instruction
+    type: text
+
+output_features:
+  - name: output
+    type: text
+
+prompt:
+  template: >-
+    Below is an instruction that describes a task, paired with an input
+    that provides further context. Write a response that appropriately
+    completes the request.
+
+    ### Instruction: {instruction}
+
+    ### Input: {input}
+
+    ### Response:
+
+generation:
+  temperature: 0.1
+  max_new_tokens: 512
+
+adapter:
+  type: lora
+
+quantization:
+  bits: 4
+
+trainer:
+  type: finetune
+  epochs: 5
+  batch_size: 1
+  eval_batch_size: 2
+  gradient_accumulation_steps: 16
+  learning_rate: 0.001
+  optimizer:
+    type: adam
+    params:
+      eps: 1.e-8
+      betas:
+        - 0.9
+        - 0.999
+      weight_decay: 0
+  learning_rate_scheduler:
+    decay: cosine
+    warmup_fraction: 0.03
+    reduce_on_plateau: 0
+"""
+)
+
+model = LudwigModel(config=qlora_fine_tuning_config, logging_level=logging.INFO)
+results = model.train(dataset=df)
+
+# Make prediction
+test_examples = pd.DataFrame([
+      {
+            "instruction": "Tell me a 200 word story about a young boy named Mini who is likes gorilla and hippopotamus.",
+            "input": ''
+      }
+])
+
+predictions = model.predict(test_examples)[0]
+for input_with_prediction in zip(test_examples['instruction'], test_examples['input'], predictions['output_response']):
+  print(f"Instruction: {input_with_prediction[0]}")
+  print(f"Input: {input_with_prediction[1]}")
+  print(f"Generated Output: {input_with_prediction[2][0]}")
+  print("\n\n")
